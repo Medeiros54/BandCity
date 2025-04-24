61b602e2a9b22416b4900ee564a1105f66e113f1
Victor Medeiros <victormedeiroschi@gmail.com>
2025-04-24 14:18:15 -0300

    Segundo Commit

--- prjArmine.py
+++ prjArmine.py
@@ -1,23 +1,24 @@
-# Boas vindas do Cliente.
-while True:
+# Começo um loop para rodar sem erro
+while True: 
+    # Boas vindas do Cliente.
     boasVindas = print('Olá, bem vindo ao BandPet Creator! Aqui você usa dois parâmetros para criar um nome para sua banda de acordo com sua cidade e seu pet! /' \
 'Está pronto pra isso?')
     aceitar = input('Você deseja continuar? Digite SIM ou NÃO. ' )
-
+    #condições para rodar corretamente 
     if aceitar == 'sim' or aceitar == 'Sim' or aceitar == 'SIM':
      print('Beleza! Vamos lá. ')
      nomeCidade = input('Digite o nome da sua Cidade: ')
      nomePet = input('Digite o nome do seu Pet: ')
-    
+    # Nome Final
      nomeBanda = nomeCidade + ' ' + nomePet    
 
      print(f'O nome de sua banda é: {nomeBanda.upper()}')
      print('Obrigado por participar! Isso é tudo!')
-     break
+     break #fim do primeiro laço
 
     elif aceitar == 'não' or aceitar == 'Não' or aceitar == 'NÃO':
       print('Que pena. Até a próxima.')
-    break
-    
-else:
-     print('Resposta Inválida, digite apenas SIM ou NÃO..')
+    break #fim do segundo laço
+     
+else: #caso a pessoa tente quebrar o programa, EU TENHO O PODER DO NÃO! 
+     print('Resposta Inválida, digite apenas SIM ou NÃO.')
